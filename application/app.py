1a185eed524932b0d5bae2ae20a41b46cbf185ca
adityasihag1996 <adityasihag1996@gmail.com>
2024-01-07 03:06:12 +0530

    FE init

--- application/app.py
+++ application/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from redis import Redis
 from rq import Queue
 import time, json
 
 app = Flask(__name__)
+CORS(app)
 
 # RQ
 redis_rq_conn = Redis(
